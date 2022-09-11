5d77a824be81575164457faa70831896be833372
najbudin007 <najbudinalam55@gmail.com>
2022-09-11 12:00:30 +0545

    Final Commit completed

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -13,7 +13,7 @@ class FrontendController extends Controller
 {
 
     public function index() {
-        $movies = Movie::latest()->get();
+        $movies = Movie::latest()->where("status","publish")->get();
         if($movies !== null) {
             return view('Frontend.layouts.index', compact('movies'));
         } else {
@@ -24,57 +24,6 @@ class FrontendController extends Controller
     // User Login
     public function login(){
         return view('Frontend.Auth.login');
-    }
-    public function loginSubmit(Request $request){
-        $data= $request->all();
-        if(Auth::attempt(['email' => $data['email'], 'password' => $data['password'],'status'=>'active'])){
-            Session::put('user',$data['email']);
-            request()->session()->notify('success','Successfully login');
-            return redirect()->route('home');
-        }
-        else{
-            request()->session()->notify('error','Invalid email and password pleas try again!');
-            return redirect()->back();
-        }
-    }
-
-    public function logout(){
-        Session::forget('user');
-        Auth::logout();
-        request()->session()-> notify('success','Logout successfully');
-        return back();
-    }
-
-    public function register(){
-        return view('frontend.pages.register');
-    }
-    public function registerSubmit(Request $request){
-        // return $request->all();
-        $this->validate($request,[
-            'name'=>'string|required|min:2',
-            'email'=>'string|required|unique:users,email',
-            'password'=>'required|min:6|confirmed',
-        ]);
-        $data=$request->all();
-        // dd($data);
-        $check=$this->create($data);
-        Session::put('user',$data['email']);
-        if($check){
-            request()->session()-> notify('success','Successfully registered');
-            return redirect()->route('home');
-        }
-        else{
-            request()->session()-> notify('error','Please try again!');
-            return back();
-        }
-    }
-    public function create(array $data){
-        return User::create([
-            'name'=>$data['name'],
-            'email'=>$data['email'],
-            'password'=>Hash::make($data['password']),
-            'status'=>'active'
-            ]);
     }
 
 }
